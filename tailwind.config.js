9c1dd8248ce9d035f2888195a8acf2cbb10c19f5
m.rahimizadeh <m.rahimizadeh49@gmail.com>
2024-06-04 22:53:08 +0330

    version_0_0_1

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./*.html"],
+  darkMode:"media",
+  theme: {
+    screens:{
+      "mobile":"0",
+      "desktop":"1000px"
+    },
+    extend: {
+    },
+  },
+  plugins: [],
+}
+
